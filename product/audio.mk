3615e914581ce60d8737d4e985354f7765282b68
Apavayan <info@apavayan.com>
2018-02-21 22:20:20 +0530

    c7prolte: Add Audio AMP bin and Mixer Path
    Change-Id: I7cad068b5ad2e8cf3a053bbb968a1eb53bad4ceb
    Signed-off-by: Apavayan <info@apavayan.com>

---
+++ product/audio.mk
@@ -0,0 +1,7 @@
+# Audio Mixer Path
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# Audio TFA-AMP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/Tfa9897.cnt:system/etc/Tfa9897.cnt
