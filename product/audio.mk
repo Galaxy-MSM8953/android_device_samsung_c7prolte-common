bed7a875ea50c5c837fbb561f9460992a92d200b
minz1 <emerytang@gmail.com>
2018-02-22 00:04:32 -0600

    Partially revert "c7prolte: Add Audio AMP bin and Mixer Path"
    * mixer_paths.xml is not device specific, and this file belongs in vendor
    
    This reverts commit c579ba53e41a3196ba010ab9b6ffb57589910292.

--- product/audio.mk
+++ product/audio.mk
@@ -1,11 +1,3 @@
-# Audio Mixer Path
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_path.xml:system/etc/mixer_path.xml
-
 # Audio TFA-AMP
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/Tfa9897.cnt:system/etc/Tfa9897.cnt \
-    $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml
-
 PRODUCT_PACKAGES += \
     audio_amplifier.msm8953
