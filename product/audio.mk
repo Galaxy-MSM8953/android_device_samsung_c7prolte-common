79ee12d3564bef603f0d94ffa91fd3597f8390b4
Apavayan <info@apavayan.com>
2018-02-23 07:39:31 +0530

    c7prolte: Add audio config for c7prolte
    Change-Id: I341598a2c8d8f86e304d32f2613b56977959005f
    Signed-off-by: Apavayan <info@apavayan.com>

--- product/audio.mk
+++ product/audio.mk
@@ -2,6 +2,11 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+	$(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
+
 # Audio TFA-AMP
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/Tfa9897.cnt:system/etc/Tfa9897.cnt \
