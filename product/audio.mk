3f29090a984a03c6d2a750c0f16ebb36894a94fe
Apavayan <info@apavayan.com>
2018-02-24 13:27:53 +0530

    c7prolte: Remove Audio Amplifer
    Change-Id: I0f0c1b19d584ab8b28fe615e8bed2628544aafa0

--- product/audio.mk
+++ product/audio.mk
@@ -12,6 +12,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/Tfa9897.cnt:system/etc/Tfa9897.cnt \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml
 
-PRODUCT_PACKAGES += \
-    audio_amplifier.msm8953 \
-    libtfa98xx
+
