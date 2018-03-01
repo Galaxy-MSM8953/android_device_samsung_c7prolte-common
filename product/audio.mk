327635ce0c9af3d87f9e2b95ac1f22d4cea76340
vm03 <vasy@vasy.ru>
2018-03-01 20:56:23 +0530

    audio_ampliifier: Add TFA98xx Audio amplifier implementation
    Change-Id: Iecace679e8684fbd5de3ce263585faab7f9ca4b0

--- product/audio.mk
+++ product/audio.mk
@@ -7,6 +7,11 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
 	$(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt
 
+# Audio NXP
+PRODUCT_PACKAGES += \
+	audio_amplifier.msm8953_32 \
+	libtfa98xx_32
+
 # Audio TFA-AMP
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/Tfa9897.cnt:system/etc/Tfa9897.cnt \
