c95102ca4403b00954190de86b810496d63ad21d
Apavayan <info@apavayan.com>
2018-03-06 01:12:55 +0530

    c7prolte-comon: Enable Nfc & FingerPrint Sensor
    Change-Id: I8d21e1fe14de1fee5d90318cc3eafb788d623305
    Signed-off-by: Apavayan <info@apavayan.com>

--- Android.mk
+++ Android.mk
@@ -23,4 +23,42 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter c7proltedd, $(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+AUTHHAT_IMAGES := \
+    authhat.b00 authhat.b01 authhat.b02 authhat.b03 authhat.b04 \
+    authhat.b05 authhat.b06 authhat.mdt
+    
+AUTHHAT_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(AUTHHAT_IMAGES)))
+$(AUTHHAT_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "AUTHHAT firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(AUTHHAT_SYMLINKS)
+
+AUTHNR_IMAGES := \
+    authnr.b00 authnr.b01 authnr.b02 authnr.b03 authnr.b04 \
+    authnr.b05 authnr.b06 authnr.mdt
+    
+AUTHNR_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(AUTHNR_IMAGES)))
+$(AUTHNR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "AUTHNR firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(AUTHNR_SYMLINKS)
+
+DUALFP_IMAGES := \
+    dualfp.b00 dualfp.b01 dualfp.b02 dualfp.b03 dualfp.b04 \
+    dualfp.b05 dualfp.b06 dualfp.mdt
+    
+DUALFP_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(DUALFP_IMAGES)))
+$(DUALFP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "DUALFP firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(DUALFP_SYMLINKS)
 endif
---
+++ product/nfc.mk
@@ -0,0 +1,22 @@
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    libnfc_nci_jni \
+    libnfc-nci \
+    NfcNci \
+    Tag
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.nfc.sec_hal=true \
+    ro.nfc.port="I2C"
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
